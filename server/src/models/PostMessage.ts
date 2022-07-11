e8d4713c31ef665e4ac385cd45315991734aaa52
husaininajib <husaininajib98@gmail.com>
2022-07-11 17:27:06 +0800

    added post schema

---
+++ server/src/models/PostMessage.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from 'mongoose';
+
+interface LikeItem {
+  type: number;
+  default: number;
+}
+interface CreatedAtItem {
+  type: string;
+  default: string;
+}
+
+interface IPost {
+  title: string;
+  message: string;
+  selectedFile: string;
+  tags: string[];
+  likeCount: LikeItem;
+  createdAt: CreatedAtItem;
+}
+
+const postSchema = new Schema<IPost>({
+  title: String,
+  message: String,
+  selectedFile: String,
+  tags: [String],
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage
